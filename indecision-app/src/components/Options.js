577f721f6485aed89031836d7cb23d3693a45313
Frozone <chineduknight@gmail.com>
2019-08-28 16:59:19 +0100

    Updated

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -3,9 +3,12 @@ import Option from './Option';
 
 const Options = props => (
   <div>
-    <button className='btn btn-link' onClick={props.handleDeleteOptions}>
-      Remove All
-    </button>
+    <div className='widget-header'>
+      <h3>Your Options</h3>
+      <button className='btn btn-link' onClick={props.handleDeleteOptions}>
+        Remove All
+      </button>
+    </div>
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
     {props.options.map(option => (
       <Option
